b560d04b647ab75c99652ea82708f5270af45534
Byron Brown <byronrb92@gmail.com>
2020-10-22 22:17:42 -0400

    setting up initial server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Hello Word");
+
+const express = require('express')
+const server = express()
+
+/* final catch-all route to index.html defined last */
+server.get('/*', (req, res) => {
+  res.send("It's Working!");
+})
+
+const port = 3000;
+server.listen(port, function() {
+  console.log('server listening on port ' + port)
+})
